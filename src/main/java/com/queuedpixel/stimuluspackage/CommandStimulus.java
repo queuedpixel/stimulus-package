0176e84b46dac5aa057306a800ffa2fab9fd5c80
Queued Pixel <git@queuedpixel.com>
2018-04-06 17:33:35 -0700

    Update /stimulus command to print out online players.

--- src/main/java/com/queuedpixel/stimuluspackage/CommandStimulus.java
+++ src/main/java/com/queuedpixel/stimuluspackage/CommandStimulus.java
@@ -1,21 +1,32 @@
 package com.queuedpixel.stimuluspackage;
 
+import java.util.Collection;
+
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandStimulus implements CommandExecutor
 {
     public boolean onCommand( CommandSender sender, Command command, String label, String[] args )
     {
-        OfflinePlayer[] players = Bukkit.getOfflinePlayers();
-        sender.sendMessage( "There are " + players.length + " players." );
+        OfflinePlayer[] offlinePlayers = Bukkit.getOfflinePlayers();
+        sender.sendMessage( offlinePlayers.length + " Offline Players:" );
 
-        for ( OfflinePlayer player : players )
+        for ( OfflinePlayer player : offlinePlayers )
         {
             sender.sendMessage( player.getName() + " : " + player.getLastPlayed() );
+        }
+
+        Collection< ? extends Player > onlinePlayers = Bukkit.getOnlinePlayers();
+        sender.sendMessage( onlinePlayers.size() + " Online Players:" );
+
+        for ( Player player : onlinePlayers )
+        {
+            sender.sendMessage( player.getName() );
         }
 
         return true;
