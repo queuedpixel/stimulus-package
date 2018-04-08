15511daa4115a475ccd675c4e59f9e1919d6c1a9
Queued Pixel <git@queuedpixel.com>
2018-04-08 14:38:54 -0700

    Create map of players to the time since they were last on the server.

--- src/main/java/com/queuedpixel/stimuluspackage/CommandStimulus.java
+++ src/main/java/com/queuedpixel/stimuluspackage/CommandStimulus.java
@@ -1,6 +1,10 @@
 package com.queuedpixel.stimuluspackage;
 
 import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
@@ -13,11 +17,19 @@ public class CommandStimulus implements CommandExecutor
 {
     public boolean onCommand( CommandSender sender, Command command, String label, String[] args )
     {
+        // current time
+        Date now = new Date();
+
+        // map of players to the number of seconds since they were last on the server
+        Map< UUID, Long > playerMap = new HashMap< UUID, Long >();
+
         OfflinePlayer[] offlinePlayers = Bukkit.getOfflinePlayers();
         sender.sendMessage( offlinePlayers.length + " Offline Players:" );
 
         for ( OfflinePlayer player : offlinePlayers )
         {
+            // store number of seconds since player was last on
+            playerMap.put( player.getUniqueId(), ( now.getTime() - player.getLastPlayed() ) / 1000 );
             sender.sendMessage( player.getUniqueId().toString() + " : " + player.getLastPlayed() );
         }
 
@@ -26,7 +38,16 @@ public class CommandStimulus implements CommandExecutor
 
         for ( Player player : onlinePlayers )
         {
+            // player is on right now, so zero seconds since they were last on the server
+            playerMap.put( player.getUniqueId(), 0l );
             sender.sendMessage( player.getUniqueId().toString() );
+        }
+
+        sender.sendMessage( playerMap.size() + " Total Players:" );
+
+        for ( UUID playerId : playerMap.keySet() )
+        {
+            sender.sendMessage( playerId.toString() + " : " + playerMap.get( playerId ));
         }
 
         return true;
